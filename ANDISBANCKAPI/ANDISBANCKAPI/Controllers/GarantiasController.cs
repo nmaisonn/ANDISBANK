47d38e65be8b006b5cc689ec6d124f8a7fe14989
Sebastian Fripp <irubyxrubyx@gmail.com>
2023-09-13 20:45:05 -0300

    garantias

---
+++ ANDISBANCKAPI/ANDISBANCKAPI/Controllers/GarantiasController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANDISBANCKAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GarantiasController : ControllerBase
+    {
+        private static readonly string[] garantias = new[]
+        {
+        "inmueble","vehiculo","terreno",
+    };
+
+        [HttpGet(Name="GetGarantias")]
+        public IEnumerable<Garantia> Get() {
+            return garantias;
+        }
+    }
+}
---
+++ ANDISBANCKAPI/ANDISBANCKAPI/Garantia.cs
@@ -0,0 +1,8 @@
+namespace ANDISBANCKAPI
+{
+    public class Garantia
+    {
+        public string Nombre { get; set; }
+
+    }
+}
