51449a243cb1b6eb598e11766544a30a192385a4
estebancasalas <esteban.casalas@correo.ucu.edu.uy>
2023-09-18 13:09:09 -0300

    Add endpoint loan/paid/{userId}

--- ANDISBANCKAPI/ANDISBANCKAPI/Controllers/LoanController.cs
+++ ANDISBANCKAPI/ANDISBANCKAPI/Controllers/LoanController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ANDISBANCKAPI.Entidades;
 
 namespace ANDIS_II.Controllers;
 
@@ -6,6 +7,13 @@ namespace ANDIS_II.Controllers;
 [Route("[controller]")]
 public class LoanController : ControllerBase
 {
+    private Loan[] _loans = new Loan[]
+    {
+        new Loan { id = 1,Descripcion = "Prestamo 1", Tipo = new LoanType{Name = "Prestamos de autos"},Monto = 1000, Tasa = 0.1, Plazo = DateTime.Now, Fecha = DateTime.Now, ClienteId = 1, Estado = 0 },
+        new Loan { id = 2,Descripcion = "Prestamo 2", Tipo = new LoanType{ Name = "Prestamos de casas"},Monto = 2000, Tasa = 0.2, Plazo = DateTime.Now, Fecha = DateTime.Now, ClienteId = 2, Estado = 1 },
+        new Loan { id = 3,Descripcion = "Prestamo 3", Tipo = new LoanType{ Name = "Prestamos bancarios"},Monto = 3000, Tasa = 0.3, Plazo = DateTime.Now, Fecha = DateTime.Now, ClienteId = 3, Estado = 2 },
+        new Loan { id = 4,Descripcion = "Prestamo 4", Tipo = new LoanType{ Name = "Prestamos de autos" },Monto = 4000, Tasa = 0.4, Plazo = DateTime.Now, Fecha = DateTime.Now, ClienteId = 4, Estado = 3 },
+    };
     public LoanController(ILogger<LoanController> logger)
     {
     }
@@ -22,10 +30,19 @@ public class LoanController : ControllerBase
 
         return arr;
     }
+
+    [HttpGet("api/v1/loan/paid/{userId}")]
+    public Loan[] GetPaidLoans(int userId)
+    {
+        // Filter and return paid loans for the specified user
+        Loan[] paidLoans = _loans.Where(x => x.ClienteId == userId && x.Estado == 1).ToArray();
+
+        return paidLoans;
+    }
 }
 
 
-public class LoanType
-{
-    public string Name { get; set; }
-}
+//public class LoanType
+//{
+//    public string Name { get; set; }
+//}
